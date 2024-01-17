fdae83d61ba6fdce40619fb1773642763e847253
king_sarp <sarpongnana9@gmail.com>
2024-01-17 23:58:07 +0000

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ServicesComponent } from './services/services.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ServicesComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
